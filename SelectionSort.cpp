a1621baddaae59a1a511cfd00172049349a173c1
dhruvgoyal09 <115341513+dhruvgoyal09@users.noreply.github.com>
2022-10-08 18:59:34 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -15,23 +15,16 @@
 
 void selectionSort(int arr[], int n)
 {
-    int i, j, max_idx;
+    int i, j;
  
     // One by one move boundary of
     // unsorted subarray
     for (i = 0; i < n-1; i++)
     {
-       
-        // Find the maximum element in
-        // unsorted array
-        max_idx = i;
-        for (j = i+1; j < n; j++)
-        if (arr[j] > arr[max_idx])
-            max_idx = j;
- 
-        // Swap the found maximum element
-        // with the first element
-        if(max_idx!=i)
-            swap(&arr[max_idx], &arr[i]);
+        for (j = i+1; j < n; j++){
+            if (arr[j] > arr[i]){
+                swap(arr[i],arr[j]);
+            }
+        }
     }
 }
